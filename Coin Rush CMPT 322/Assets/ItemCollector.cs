afdcd77e5ac94622c23c660ff79177b9293fb2b6
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-03-20 18:25:11 -0600

    Random integers made and shown

--- Coin Rush CMPT 322/Assets/ItemCollector.cs
+++ Coin Rush CMPT 322/Assets/ItemCollector.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
     private int collectables = 0;
 
     [SerializeField] private Text collectablesText;
+    [SerializeField] private TextMeshProUGUI randomNumbersText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Collectable"))
         {
+            int randX;
+            int randY;
+            do {
+            randX = Random.Range(1,9);
+            randY = Random.Range(1,10);
+            } while (randX + randY > 9);
+            randomNumbersText.text = randX + " + " + randY + " =";
+            Debug.Log("X = " + randX + ", Y = " + randY);
             Destroy(collision.gameObject);
             collectables++;
             Debug.Log("Items: " + collectables);
             collectablesText.text = "Items: " + collectables;
         }
+	
+	
     }
 }
