c905ca6de8cceaf698cfe7f45b9be7c9dffd28c2
Omar A <114465136+omaroaalj@users.noreply.github.com>
2023-03-21 01:07:39 -0600

    Math problems working!

--- Coin Rush CMPT 322/Assets/ItemCollector.cs
+++ Coin Rush CMPT 322/Assets/ItemCollector.cs
@@ -1,34 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int collectables = 0;
+    private static bool charCollided = false;
 
-    [SerializeField] private Text collectablesText;
-    [SerializeField] private TextMeshProUGUI randomNumbersText;
+    public static bool getCollisionPresent() {
+        return charCollided;
+    }
+
+    public static void setCollisionPresent(bool collidedCondition) {
+        charCollided = collidedCondition;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Collectable"))
         {
-            int randX;
-            int randY;
-            do {
-                randX = Random.Range(1,9);
-                randY = Random.Range(1,10);
-            } while (randX + randY > 9);
-            randomNumbersText.text = randX + " + " + randY + " =";
-            Debug.Log("X = " + randX + ", Y = " + randY);
+            MathKeypadInput.setRandomNumbers();
+            charCollided = true;
             Destroy(collision.gameObject);
-            collectables++;
-            Debug.Log("Items: " + collectables);
-            collectablesText.text = "Items: " + collectables;
         }
-	
-	
     }
 }
--- Coin Rush CMPT 322/Assets/MathKeypadInput.cs
+++ Coin Rush CMPT 322/Assets/MathKeypadInput.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MathKeypadInput : MonoBehaviour
 {
-    private int userAnswer;
+    private int userAnswer = -1;
+    [SerializeField] private TextMeshProUGUI randomNumbersText;
+    [SerializeField] private GameObject mathUI;
+    [SerializeField] private Text collectablesText;
+    private int collectables = 0;
+    private static int randomX;
+    private static int randomY;
+    private static int correctAnswer;
 
     public void setUserAnswer(int buttonInput) {
         userAnswer = buttonInput;
@@ -12,5 +21,42 @@ public class MathKeypadInput : MonoBehaviour
 
     public int getUserAnswer() {
         return userAnswer;
+    }
+
+    public static void setRandomNumbers() {
+        do {
+            randomX = Random.Range(1,9);
+            randomY = Random.Range(1,10);
+        } while (randomX + randomY > 9);
+        correctAnswer = randomX + randomY;
+        Debug.Log("Random numbers: " + randomX + " and " + randomY);
+    }
+
+    void Start() {
+        Debug.Log("Starting math UI...");
+        mathUI.SetActive(false);
+    }
+
+    void Update() {
+        if (ItemCollector.getCollisionPresent()) {
+            if (userAnswer <= -1) {
+                mathUI.SetActive(true);
+                Time.timeScale = 0f;
+                randomNumbersText.text = randomX + " + " + randomY + " =";
+            } else {
+                Time.timeScale = 1f;
+                if (userAnswer == correctAnswer) {
+                    Debug.Log("Correct Answer!");
+                    collectables++;
+                    collectablesText.text = "Items: " + collectables;
+                }
+                else {
+                    Debug.Log("Incorrect Answer.");
+                }
+                mathUI.SetActive(false);
+                userAnswer = -1;
+                ItemCollector.setCollisionPresent(false);
+            }
+        }
     }
 }
